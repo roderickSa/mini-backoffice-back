812aa619d88b21c42333dea20b8e5a8c37ea4355
roderickSanchez <roderick.sanchez@justo.mx>
2024-03-28 15:23:37 -0500

    change default response to CategoryResource

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use DateTime;
 
 class CategoryResource extends JsonResource
 {
@@ -14,6 +15,11 @@ class CategoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'created_at' => (new DateTime($this->created_at))->format("Y-m-d H:i:s"),
+            'updated_at' => (new DateTime($this->updated_at))->format("Y-m-d H:i:s"),
+        ];
     }
 }
